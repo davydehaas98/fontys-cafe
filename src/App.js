74155a7526e0da6104f70801c2c7ce9b03eb684f
Davy de Haas <davy.dehaas98@gmail.com>
2018-11-27 19:20:53 +0100

    Fix text size

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>Is Fontys cafe open?</p>
-          <h1>JA!</h1>
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+        <h1>Is het Fontys cafe open?</h1>
+        <h2>Misschien?</h2>
+        <img src={logo} className="App-logo" alt="logo" />
       </div>
     )
   }
