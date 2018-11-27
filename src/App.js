affb65060bb020d08bace8ef3cde14393c54caf3
Davy de Haas <davy.dehaas98@gmail.com>
2018-11-27 14:36:22 +0100

    Add ja

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Is het al tijd voor bier?</p>
+          <h1>JA.</h1>
         </header>
       </div>
     )
