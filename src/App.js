a6e1fe29b81cfc01b22d713e2b2696653a15cfb9
Davy de Haas <davy.dehaas98@gmail.com>
2018-12-01 12:32:10 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import logo from './beer.gif'
+import image from './beer.gif'
 import './App.css'
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>Is het Fontys cafe open?</h1>
         <h2>Misschien?</h2>
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={image} />
       </div>
     )
   }
